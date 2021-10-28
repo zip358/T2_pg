8e14660c3880fc56d43e008d3767db6b432a7fdd
zip358com <71567084+zip358@users.noreply.github.com>
2021-10-28 15:25:29 +0900

    T2がコードを10年書いている mouseover

--- mouseover/assets/js/main.js
+++ mouseover/assets/js/main.js
@@ -4,7 +4,11 @@ for (const key in img) {
     if (Object.hasOwnProperty.call(img, key)) {
         const element = img[key];
         element.addEventListener("mouseover",function(){
-            let url = "https://picsum.photos/id/" + this.getAttribute("data-id") + "/300/400";
+            let url = "https://picsum.photos/id/" + this.getAttribute("data-id").in + "/300/400";
+            this.src = url;
+        });
+        element.addEventListener("mouseout",function(){
+            let url = "https://picsum.photos/id/" + this.getAttribute("data-id").out + "/300/400";
             this.src = url;
         });
     }
