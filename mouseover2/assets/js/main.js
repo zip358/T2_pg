659c255d7133d40a34eeedfdd19f3f6fb556255e
zip358com <71567084+zip358@users.noreply.github.com>
2021-10-31 07:50:31 +0900

    T2がコードを10年書いている mouseover2

--- mouseover2/assets/js/main.js
+++ mouseover2/assets/js/main.js
@@ -1,5 +1,5 @@
 document.body.addEventListener("mouseover",function(e){
-    console.log(e);
+    //console.log(e);
     document.getElementById("X").innerText = e.screenX;
     document.getElementById("Y").innerText = e.screenY;
     document.body.style.backgroundColor = "RGB(" +  Math.floor(e.screenY % 255) +"," +  Math.floor(e.screenX % 255) +"," +  Math.floor(e.screenY % 255) +")";
