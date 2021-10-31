530b3c9c4d4846ab8e842d649d22c522029f3455
zip358com <71567084+zip358@users.noreply.github.com>
2021-11-01 05:43:23 +0900

    T2がコードを10年書いている 干支

---
+++ eto/assets/js/main.js
@@ -0,0 +1,26 @@
+var eto = [
+    {no:4,eto:"子",yomi:"ね"},
+    {no:5,eto:"丑",yomi:"うし"},
+    {no:6,eto:"寅",yomi:"とら"},
+    {no:7,eto:"卯",yomi:"う"},
+    {no:8,eto:"辰",yomi:"たつ"},
+    {no:9,eto:"巳",yomi:"み"},
+    {no:10,eto:"午",yomi:"うま"},
+    {no:11,eto:"未",yomi:"ひつじ"},
+    {no:0,eto:"申",yomi:"さる"},
+    {no:1,eto:"酉",yomi:"とり"},
+    {no:2,eto:"戌",yomi:"いぬ"},
+    {no:3,eto:"亥",yomi:"い"}
+];
+
+document.getElementById("eto").innerText = (function(){
+
+    let d = new Date();
+    str = "";
+    for (const key in eto) {
+        if(eto[key].no ===(d.getFullYear % 12)){
+           str = eto[key].eto + `[${eto[key].yomi}]`;
+        }
+    }
+    return str;
+}());
